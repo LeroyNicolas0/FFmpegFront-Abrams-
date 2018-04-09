604963300904ab5b80c1ab49d48ddf3f6ed78978
Rylux <ryckaert.guillaume@neuf.fr>
2018-04-09 18:02:53 -0400

    struct 2

--- src/Enums/Extension.java
+++ src/Enums/Extension.java
@@ -1,5 +1,27 @@
 package Enums;
 
 public enum Extension {
+	//Audio
+	MP3("mp3","MPEG-Layer 3"),
+	//Video
+	MP4("mp4","MPEG-Layer 4 "),
+	//Sous-Titres
+	SRT("srt","");
+	//Autres
+	
+	
+	private String ext;
+	private String description;
+	
+	Extension(String ext, String description){
+		this.ext=ext;
+		this.description=description;
+	}
+	public String get_ext() {
+		return this.ext;
+	}
+	public String get_description() {
+		return this.description;
+	}
 
 }
