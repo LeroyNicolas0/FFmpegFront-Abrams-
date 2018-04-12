5b3e24f326eb53dae651b7244d649ffd2a351ab1
MANZANILLAF <manzanilla.francis@gmail.com>
2018-04-12 15:21:37 -0400

    bouton choix vidéo

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -1,5 +1,39 @@
 package application;
+import java.io.File;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 public class SampleController {
+	@FXML
+	//Bouton pour choisir le fichier vidéo
+	private Button browse;
+	@FXML
+	private ListView<String> view;
 	
+	//Méthode pour choisir le fichier vidéo
+	public String ButtonBrowseAction(ActionEvent event) {
+		FileChooser fc = new FileChooser();
+		fc.getExtensionFilters().addAll(
+					new ExtensionFilter("MP4", "*.mp4"),
+					new ExtensionFilter("GP", "*.3gp"),
+					new ExtensionFilter("G2", "*.3g2"),
+					new ExtensionFilter("MKV", "*.mkv"),
+					new ExtensionFilter("OGV", "*.ogv"),
+					new ExtensionFilter("AVI", "*.avi"));
+					
+		File video = fc.showOpenDialog(null);
+		if(video != null) {
+			view.getItems().add(video.getAbsolutePath());
+			return video.getAbsolutePath();
+		}
+		else {
+			System.out.println("the file is not a video");
+			return null;
+		}		
+	}
 }
