ca2cf0e219bf269cfdebe6ec5d24d7bd562544a1
MANZANILLAF <manzanilla.francis@gmail.com>
2018-04-14 17:42:55 -0400

    Auto stash before merge of "master" and "origin/master"

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -2,20 +2,33 @@ package application;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import data.Source;
 
+import data.Source;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
+import jdk.nashorn.internal.runtime.options.Options;
 
 public class SampleController {
+	
+	ObservableList<String> video_extension_list = FXCollections
+				.observableArrayList("MP4", "GP", "G2", "MKV", "AVI");
+	ObservableList<String> audio_codec_list = FXCollections
+			.observableArrayList();
+	ObservableList<String> video_codec_list = FXCollections
+			.observableArrayList();
 	
 	//Bouton pour choisir le fichier vidéo
 	@FXML
@@ -24,6 +37,19 @@ public class SampleController {
 	//Bouton pour choisir le(s) fichier(s) audio
 	@FXML
 	private Button browse_audio;
+	
+	//Choix de l'extension;
+	@FXML
+	private ChoiceBox box_extension;
+	
+	//Choix du codec audio;
+	@FXML
+	private ChoiceBox box_audio;
+		
+	//Choix du codec vidéo;
+	@FXML
+	private ChoiceBox box_video;
+	
 	
 	//Bouton pour choisir le(s) sous-titre(s)
 	@FXML
@@ -109,5 +135,48 @@ public class SampleController {
 		else {
 			System.out.println("the file is not a subtitle");
 		}		
+	}
+	
+	public void initialize() 
+	{
+		box_extension.setValue("");
+		box_video.setValue("");
+		box_extension.setItems(video_extension_list);
+		
+		
+		box_extension.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+		      @Override
+		      public void changed(ObservableValue<? extends Number> observableValue, Number old_value, Number new_value) {
+		    	String option;
+		        System.out.println(box_extension.getItems().get((Integer) new_value));
+		        option = (String) box_extension.getItems().get((Integer) new_value);
+		        switch (option)
+		        {
+		        case "MP4":
+		        	box_video.setItems(FXCollections.observableArrayList("LIBX264", "LIBX265", "LIBXVID"));
+		        	box_audio.setItems(FXCollections.observableArrayList("AAC", "MP3LAME"));
+		        	break;
+		        case "GP":
+		        	box_video.setItems(FXCollections.observableArrayList("LIBX264", "LIBXVID"));
+		        	box_audio.setItems(FXCollections.observableArrayList("AAC"));
+		        	break;
+		        case "G2":
+		        	box_video.setItems(FXCollections.observableArrayList("LIBX264", "LIBXVID"));
+			        box_audio.setItems(FXCollections.observableArrayList("AAC"));
+		        	break;
+		        case "MKV":
+		        	box_video.setItems(FXCollections.observableArrayList("LIBX264", "LIBX265", "LIBXVID"));
+		        	box_audio.setItems(FXCollections.observableArrayList("VORBIS", "OPUS", "MP3LAME", "FLAC", "AAC"));
+		        	break;
+		        case "AVI":
+		        	box_video.setItems(FXCollections.observableArrayList("LIBXVID"));
+		        	box_audio.setItems(FXCollections.observableArrayList("MP3LAME"));
+		        	break;
+		        default:
+		        	break;
+		        }
+		      }
+
+		    });
 	}
 }
